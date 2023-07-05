38b067060566b5a5a8eee1e11668483043998f9f
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-06 01:37:16 +0530

    Create 122. Best Time to Buy and Sell Stock II(.cpp

---
+++ 122. Best Time to Buy and Sell Stock II(.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int ans=0;
+        int n=p.size();
+        for(int i=1;i<n;++i){
+            if(p[i-1]<p[i]){
+                ans+=p[i]-p[i-1];
+                i++;
+            }
+        }
+        return ans;
+    }
+};
+// T.C=O(n)
+// S.C=O(1)
