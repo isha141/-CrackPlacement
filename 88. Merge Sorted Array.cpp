2650da81511ce879d8a600030fa363baacf9f823
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-07-08 01:46:00 +0530

    Create 88. Merge Sorted Array.cpp

---
+++ 88. Merge Sorted Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+        int k=(n+m-1);
+        int i=m-1;
+        int j=n-1;
+        while(i>=0 && j>=0){
+            if(n1[i]>=n2[j]){
+                n1[k--]=n1[i--];
+            }
+            else{
+                n1[k--]=n2[j--];
+            }
+        }
+        while(j>=0){
+            n1[k--]=n2[j--];
+        }
+        return;
+    }
+}; 
+
+// T.c=O(n)
+// S.c=O(1)
