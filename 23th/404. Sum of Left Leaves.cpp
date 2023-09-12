acea6cb7eedee87470530409c231258ba35acfbf
Isha Jindal <Ishajindal_20022245.cse@geu.ac.in>
2023-09-12 15:16:43 +0530

    Create 404. Sum of Left Leaves.cpp

---
+++ 23th/404. Sum of Left Leaves.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution { 
+      int sum=0;
+    int solve(TreeNode *root){
+          if(root==NULL)
+               return 0;
+          // if(root->left==NULL && root->right==NULL)
+          //       return 0;
+          solve(root->left);
+          if(root){
+                if(root->left!=NULL && root->left->right==NULL && root->left->left==NULL){
+                    sum+=root->left->val;
+                }
+           }
+          solve(root->right);
+           
+         return 0;
+    }
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+          if(root==NULL)
+               return sum;
+          solve(root);
+          return sum;
+    }
+};
+
+// T.C=O(n)
+// S.C=o(1)
